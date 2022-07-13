3fb76917652ce44d579adb7c2cc545f67eabf80a
Udoh Augustina <mariaassumpta07@gmail.com>
2022-07-13 11:47:31 -0700

    altschool javascript assignment

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const assignment = require('./assignment.js');
 
 const { sumOfNumbers, countEvenNumbers, celsiusToFahrenheit } = assignment;
 
-if (sumOfNumbers && typeof sumOfNumbers === 'function'){
+if (sumOfNumbers && typeof sumOfNumbers === 'function') {
     console.log(sumOfNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 }
 
-if (countEvenNumbers && typeof countEvenNumbers === 'function'){
+if (countEvenNumbers && typeof countEvenNumbers === 'function') {
     console.log(countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 }
 
-if (celsiusToFahrenheit && typeof celsiusToFahrenheit === 'function'){
+if (celsiusToFahrenheit && typeof celsiusToFahrenheit === 'function') {
     console.log(celsiusToFahrenheit([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 }
